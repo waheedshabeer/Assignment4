5c8c6b9af182fd4630edaa43ad19231ec3220789
syed-shoaib-ali <63425832+waheedshabeer@users.noreply.github.com>
2022-06-20 11:54:23 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Main from './screens/Main';
+import Detail from './screens/Detail';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={Detail}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
